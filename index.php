6aba36ff616398c242d2776a1797ae01fec718a8
Jorge Moreno <alterebro@gmail.com>
2020-10-22 23:42:33 +0200

    minify and gzip output

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+$cube = file_get_contents( "cube.html" );
+$cube = preg_replace('#<!--(.|\s)*?-->#', '', $cube);
+$cube = preg_replace('#(?ix)(?>[^\S ]\s*|\s{2,})(?=(?:(?:[^<]++|<(?!/?(?:textarea|pre)\b))*+)(?:<(?>textarea|pre)\b|\z))#', '', $cube);
+
+header("Content-Type: text/html; charset=UTF-8");
+ob_start("ob_gzhandler");
+
+    echo $cube;
+
+ob_end_flush();
