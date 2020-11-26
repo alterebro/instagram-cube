aa502aad1d699baa74a4b9c93eceb5d45ac79c23
Jorge Moreno <alterebro@gmail.com>
2020-11-26 01:13:37 +0100

    Send custom title on hashchange

--- cube/js/gtag.js
+++ cube/js/gtag.js
@@ -1,8 +1,11 @@
 window.dataLayer = window.dataLayer || [];
 function gtag(){ dataLayer.push(arguments); }
 gtag('js', new Date());
-gtag('config', 'G-22DMFKLBKF', {'page_path': location.pathname + location.search + location.hash });
+gtag('config', 'G-22DMFKLBKF');
 
 window.addEventListener("hashchange", e => {
-    gtag('config', 'G-22DMFKLBKF', {'page_path': location.pathname + location.search + location.hash });
+    gtag('config', 'G-22DMFKLBKF', {
+        'page_title' : document.title + ' (' + location.hash + ')',
+        'page_path': location.pathname + location.search + location.hash
+    });
 });
