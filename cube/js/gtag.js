99a7d4dea5e3808d9a50af4a7718e896d079acf1
Jorge Moreno <alterebro@gmail.com>
2020-11-26 01:04:39 +0100

    Fix multiple send pageviews

--- cube/js/gtag.js
+++ cube/js/gtag.js
@@ -1,7 +1,6 @@
 window.dataLayer = window.dataLayer || [];
 function gtag(){ dataLayer.push(arguments); }
 gtag('js', new Date());
-gtag('config', 'G-22DMFKLBKF');
 gtag('config', 'G-22DMFKLBKF', {'page_path': location.pathname + location.search + location.hash });
 
 window.addEventListener("hashchange", e => {
